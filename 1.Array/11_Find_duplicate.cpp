a02feb84fc4f09154c0ed2a5ed16965d3c4399c2
aggarwal-shivam <ashivam093@gmail.com>
2021-02-03 00:26:54 +0530

    find duplicates

--- 1.Array/11_Find_duplicate.cpp
+++ 1.Array/11_Find_duplicate.cpp
@@ -12,3 +12,20 @@ int findDuplicate_map(vector<int>& nums) {
         }
         return ans;
     }
+
+#slow-fast pointer approach
+int findDuplicate(vector<int>& nums) {
+        int fast=0, slow=0;
+        fast=nums[nums[fast]];
+        slow=nums[slow];
+        while(fast!=slow){
+            fast=nums[nums[fast]];
+            slow=nums[slow];
+        }
+        fast=0;
+        while(fast!=slow){
+            fast=nums[fast];
+            slow=nums[slow];
+        }
+        return fast;
+    }
